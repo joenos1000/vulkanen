c5c44c792ffbf42d0352e8b61967366cfaf798f5
joenos <jonaspipper@hotmail.com>
2025-10-03 23:29:56 +0200

    remove text on homepage and minor text changes

--- vulkanen/src/app/page.tsx
+++ vulkanen/src/app/page.tsx
@@ -2,9 +2,6 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white">
       <section className="container px-4 py-24 text-center">
-        <h1 className="text-4xl sm:text-6xl font-extrabold tracking-tight text-gray-900">
-          Fra skole til samlingssted - et nyt kapitel for Ørding og Mors
-        </h1>
       </section>
     </main>
   );
